0906c5e25fff52845d8837766f43eae8a913d892
heppysingh <nitusingh5457@gmail.com>
2026-09-26 16:41:26 +0000

    change ROOTFS_PATH to /dev/block/by-name/super

--- recovery/root/system/bin/boot-ubuntu.sh
+++ recovery/root/system/bin/boot-ubuntu.sh
@@ -13,7 +13,7 @@ DS_FLAGS="--hw-access --privileged=full -B /tmp:/recovery --foreground"
 # block devices like /dev/block/* (SD cards, partitions).
 # If you want to use a directory-based rootfs, simply change
 # -i ${ROOTFS_PATH} to -r ${ROOTFS_PATH} in the main command.
-ROOTFS_PATH=/data/local/rootfs.img
+ROOTFS_PATH=/dev/block/by-name/super
 
 # Main execution
 exec ${RECOVERY_CONSOLE_PATH} \
